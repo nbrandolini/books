61ba854f2db4e9c67863492c25c7242e47bed486
CheezItMan <chris@adadevelopersacademy.org>
2018-03-19 16:31:52 -0700

    added views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,21 @@
 class BooksController < ApplicationController
 
+  BOOKS_LIST = [
+    {id: 1, title: "1984", author: "George Orwell"},
+    {id: 2, title: "Pride & Prejudice", author: "Jane Austin"},
+    {id: 3, title: "Harry Potter", author: "JK Rowling"},
+    {id: 4, title: "Harry Poodr", author: "Sandy Metz"}
+  ]
+
   def index
+    @books = BOOKS_LIST
   end
-  
+
   def show
+    id = params[:id]
+    @book = BOOKS_LIST.find do |book|
+      book[:id] == id.to_i
+    end
   end
 
   def new
