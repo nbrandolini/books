2899a62e40e5633e1c011a14cf958877620abbe3
CheezItMan <chris@adadevelopersacademy.org>
2018-03-20 13:37:51 -0700

    Added Database to controller/views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,12 @@
 class BooksController < ApplicationController
 
-  BOOKS_LIST = [
-    {id: 1, title: "1984", author: "George Orwell"},
-    {id: 2, title: "Pride & Prejudice", author: "Jane Austin"},
-    {id: 3, title: "Harry Potter", author: "JK Rowling"},
-    {id: 4, title: "Harry Poodr", author: "Sandy Metz"}
-  ]
-
   def index
-    @books = BOOKS_LIST
+    @books = Book.all
   end
 
   def show
     id = params[:id]
-    @book = BOOKS_LIST.find do |book|
-      book[:id] == id.to_i
-    end
+    @book = Book.find(id)
   end
 
   def new
@@ -31,7 +22,15 @@ class BooksController < ApplicationController
   end
 
   def destroy
+    id = params[:id]
+    @book = Book.find(id)
+    @book.destroy
   end
+
+
+
+
+
 
 
 end
