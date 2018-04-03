13687e9c84a9e6f8d037543a5a88dda2262606a7
CheezItMan <chris@adadevelopersacademy.org>
2018-04-03 07:50:38 -0700

    added seeds

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,11 @@
 class Author < ApplicationRecord
   has_many :books
+
+  def first_published
+    my_books = self.books.order(:publication_year)
+
+    first_book = my_books.first
+
+    return first_book.publication_year
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+author_list = [
+  {
+    name: "J.K. Rowling"
+  },
+  {
+    name: "Stephen King"
+  },
+  {
+    name: "William Shakesphere"
+  }
+]
+
+author_list.each do |author|
+  worked = Author.create(author)
+  puts "#{author[:name]} has ID: #{worked.id}"
+end
+
+puts "\n======\n"
+
+book_list = [
+  {title: "Game of Thrones", description: "Hyper Violent", author_id: Author.first.id},
+  {title: "99 Bottles of OOP", description: "Awesome", author_id: Author.last.id},
+  {title: "Wizard of Oz", description: "Allegory", author_id: Author.all.sample.id},
+]
+
+book_list.each do |book|
+  book = Book.create(book)
+  puts "#{book[:title]} has ID: #{book.id}"
+end
+
+
+#
